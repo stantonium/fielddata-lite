ce9d3bdbbc64d57f92b345e91d0b3945fcfe24f5
Dan Hopp <exnehilo7@gmail.com>
2023-01-31 17:08:09 -0500

    Initial Commit

---
+++ TestConnection/Models.swift
@@ -0,0 +1,12 @@
+//
+//  Models.swift
+//  TestConnection
+//
+//  Created by Hopp, Dan on 1/31/23.
+//
+
+import Foundation
+
+class ResponseModel: Codable, Identifiable {
+    var type: String? = ""
+}
---
+++ TestConnection/TestConnectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestConnectionApp.swift
+//  TestConnection
+//
+//  Created by Hopp, Dan on 1/31/23.
+//
+
+import SwiftUI
+
+@main
+struct TestConnectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
