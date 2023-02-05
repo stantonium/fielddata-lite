5bb0c8fef4c43322474987da1fb407f9ea59e63a
o9h <hoppdj@ornl.gov>
2023-02-05 18:56:05 -0500

    Try SwiftUI POST

---
+++ php/searchOrgNameByArea.php
@@ -0,0 +1,29 @@
+<?php
+
+    // get login file
+    require_once "login.php";
+
+    // log into DB
+    $dbConn = logIntoPostgreSQLroutes();
+
+    $colName = htmlentities($_POST['_column_name']);
+    $colValue = htmlentities($_POST['_column_value']);
+    $orgName = htmlentities($_POST['_org_name']);
+
+    $result = pg_query_params($dbConn, 
+    "SELECT * FROM query_search_by_area($1, $2, $3)", array($colName, $colValue,
+    $orgName));
+
+    // variable for result
+    $data = array();
+
+    // get rows. Do not use pg_fetch_row(). Do not include numerical indices.
+    while ($row = pg_fetch_array($result, NULL, PGSQL_ASSOC))
+    {
+        array_push($data, $row);
+    }
+
+    // send JSON response
+    echo json_encode($data);
+
+?>
