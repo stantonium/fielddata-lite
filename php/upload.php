2a1386cdb357c1dee5b1beda1eb0a8b5d4331e2b
o9h <hoppdj@ornl.gov>
2024-03-14 16:51:00 -0400

    Add is uploaded. Silence warnings.

--- php/upload.php
+++ php/upload.php
@@ -1,69 +1,86 @@
 <?php  //code from https://jmsliu.com/3224/ios-upload-image-from-gallery-or-camera-to-server-side.html
 
-$uploadFolder = "./upload";
+$fileSavePath = "";
+$fileName = "";
 
-if (!file_exists($uploadFolder)) {
-
-    mkdir($uploadFolder);
-
+if (!empty($_POST["fileSavePath"]) && !empty($_POST["fileName"])) {
+    $fileSavePath = htmlentities($_POST["fileSavePath"]);
+    $fileName = htmlentities($_POST["fileName"]);
 }
 
- 
+$uploadFolder = "./upload";
 
-if (is_array($_FILES["file"])) {
+processFile($fileSavePath, $fileName, $uploadFolder);
 
-    $numberOfFiles = 1; // count($_FILES["file"]["name"]);
+function processFile($fileSavePath, $fileName, $uploadFolder) {
 
-    for ($i = 0; $i < $numberOfFiles; $i++) { //ignore this comment >
+    // Create folder if it doesn't exist
+    $uploadFile = $uploadFolder . "/" . $fileSavePath;
+    if (!file_exists($uploadFile)) {
+        mkdir($uploadFile, 0744, true);
+    }
+    $uploadFile = $uploadFile . "/" . $fileName;
 
-        $path_parts = pathinfo($_FILES["file"]["tmp_name"]);
+    doesFileExist($uploadFile);
+}
 
-        // Create device and trip subfolders
-        $uploadFile = $uploadFolder . "/" . htmlentities($_POST["fileSavePath"]);
+ // See if file already exists on the server
+function doesFileExist($uploadFile) {
+    if (file_exists($uploadFile)) {
 
-        if (!file_exists($uploadFile)) {
+        echo json_encode(["Message" => "file exists!",
+                            "Status" => "Error"]);
+        exit;
 
-            mkdir($uploadFile, 0744, true);
+    } else {
+        uploadFile($uploadFile);
+    }
+}
 
-        }
+function uploadFile($uploadFile) {
+    if (!empty($_FILES["file"])) {
+        if (is_array($_FILES["file"])) {
 
-        // Create save path and filename
-        $uploadFile = $uploadFile . "/" .  basename($_FILES["file"]["name"]);
+            $numberOfFiles = 1; // count($_FILES["file"]["name"]);
 
-        $imageFileType = strtolower(pathinfo($uploadFile, PATHINFO_EXTENSION));
+            for ($i = 0; $i < $numberOfFiles; $i++) { //ignore this comment >
 
-        // $lat = $_POST["lat"];
-        // $long = $_POST["long"];
+                $imageFileType = strtolower(pathinfo($uploadFile, PATHINFO_EXTENSION));
 
-       // if (!(getimagesize($_FILES["file"]["tmp_name"][$i]) !== false)) {
-       //     echo "Sorry, your image is invalid";
-       //     exit;
-       // }
+                // $lat = $_POST["lat"];
+                // $long = $_POST["long"];
 
-        if ($_FILES["file"]["size"] > 10000000) {
+            // if (!(getimagesize($_FILES["file"]["tmp_name"][$i]) !== false)) {
+            //     echo "Sorry, your image is invalid";
+            //     exit;
+            // }
 
-            echo "Sorry, your file is too large.";
-            exit;
+                if ($_FILES["file"]["size"] > 50000000) {
 
-        }
+                    echo "Sorry, your file is too large.";
+                    exit;
 
-        // if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" && #imageFileType !=".csv" && imageFileType !=".heic") {
-        //    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-        //    exit;
-        // }
+                }
 
-        if (move_uploaded_file($_FILES["file"]["tmp_name"], $uploadFile)) {
+                // if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" && #imageFileType !=".csv" && imageFileType !=".heic") {
+                //    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+                //    exit;
+                // }
 
-            echo json_encode(["Message" => "Upload image ".basename($_FILES["file"]["name"])." successfully!",
-                                 "Status" => "OK" //,
-                                // "lat" => $_REQUEST["lat"],
-                                // "long" => $_REQUEST["long"]
-                                ]);
+                if (move_uploaded_file($_FILES["file"]["tmp_name"], $uploadFile)) {
 
-        } else {
+                    echo json_encode(["Message" => "Upload image ".basename($_FILES["file"]["name"])." successfully!",
+                                        "Status" => "OK" //,
+                                        // "lat" => $_REQUEST["lat"],
+                                        // "long" => $_REQUEST["long"]
+                                        ]);
 
-            echo "Sorry, there was an error uploading your file.";
+                } else {
 
+                    echo "Sorry, there was an error uploading your file.";
+
+                }
+            }
         }
     }
 }
