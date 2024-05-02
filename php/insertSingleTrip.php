b30ab1f0aef4d463a0487551a78af3122c8ea31f
o9h <hoppdj@ornl.gov>
2024-05-02 17:04:45 -0400

    Insert single point for Trip

---
+++ php/insertSingleTrip.php
@@ -0,0 +1,46 @@
+<?php
+
+    $tripName = $picUUID = $gps = $hdop = $longtitude = $latitude = "";
+    $altitude = $scannedText = $timeStamp = $notes = "";
+
+ #   if (!empty($_POST["_name"]) && !empty($_POST["_pic_uuid"]) && !empty($_POST["_gps"]) &&
+ #       !empty($_POST["_hdop"]) && !empty($_POST["_long"]) && !empty($_POST["_lat"]) &&
+ #       !empty($_POST["_alt"]) && !empty($_POST["_scanned_text"]) && !empty($_POST["_time"]) &&
+ #       !empty($_POST["_notes"])) {
+
+        $tripName = htmlentities($_POST["_name"]);
+        $picUUID = htmlentities($_POST["_pic_uuid"]);
+        $gps = htmlentities($_POST["_gps"]);
+        $hdop = htmlentities($_POST["_hdop"]);
+        $longtitude = htmlentities($_POST["_long"]);
+        $latitude = htmlentities($_POST["_lat"]);
+        $altitude = htmlentities($_POST["_alt"]);
+        $scannedText = htmlentities($_POST["_scanned_text"]);
+        $timeStamp = htmlentities($_POST["_time"]);
+        $notes = htmlentities($_POST["_notes"]);
+
+  #  } else {exit;}
+
+    // get login file
+    require_once "login.php";
+
+    // log into DB
+    $dbConn = logIntoPostgreSQLroutes();
+
+    $result = pg_query_params($dbConn, 
+    "call fern.insert_row_into_trip($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)",
+    array($tripName, $picUUID, $gps, $hdop, $longtitude, $latitude, $altitude, $scannedText, $timeStamp, $notes));
+
+    // variable for result
+    $data = array();
+
+    // get rows. Do not use pg_fetch_row(). Do not include numerical indices.
+    while ($row = pg_fetch_array($result, NULL, PGSQL_ASSOC))
+    {
+        array_push($data, $row);
+    }
+
+    // send JSON response
+    echo json_encode($data);
+
+?>
